26e22448d56d9707b364236d8ea99cf5271a13e7
ClariVM <100391814+ClariVM@users.noreply.github.com>
2022-08-30 22:11:16 -0300

    experiencia laboral

---
+++ ProyectoIntegrador/Back-End/claribel/src/main/java/com/portfolio/claribel/Security/Entity/Rol.java
@@ -0,0 +1,48 @@
+package com.portfolio.claribel.Security.Entity;
+
+import com.portfolio.claribel.Security.Enums.RolNombre;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Rol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int Id;
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private RolNombre rolNombre;
+    
+    //constructor
+
+    public Rol() {
+    }
+
+    public Rol(RolNombre rolNombre) {
+        this.rolNombre = rolNombre;
+    }
+    
+    //getter and setter
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public RolNombre getRolNombre() {
+        return rolNombre;
+    }
+
+    public void setRolNombre(RolNombre rolNombre) {
+        this.rolNombre = rolNombre;
+    }
+    
+}
